ecfbf543d36c374fe8c6f535cac0d47d67d5147c
Paresh Solanki <solankiparesh1183@gmail.com>
2026-06-09 20:00:25 +0530

    feat: update Analytics and layout components to load GTM and Clarity scripts with improved strategy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -82,6 +82,19 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <Script
+          id="gtm-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer', '${GTM_CONTAINER_ID}');
+            `,
+          }}
+        />
       </head>
       <body className={`${instrumentSans.variable} ${spaceGrotesk.variable} font-sans antialiased`} suppressHydrationWarning>
         <OrganizationSchema />
--- src/components/common/Analytics.tsx
+++ src/components/common/Analytics.tsx
@@ -7,25 +7,17 @@ const GTM_ID = process.env.NEXT_PUBLIC_GTM_CONTAINER_ID || "GTM-WM9HRJVV";
 const CLARITY_ID = process.env.NEXT_PUBLIC_MS_CLARITY_ID;
 
 export default function Analytics() {
+  if (!CLARITY_ID) return null;
+
   return (
     <>
-      {/* GTM Script */}
-      <Script
-        id="gtm-init"
-        strategy="beforeInteractive"
-      >
-        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');`}
-      </Script>
-
       {/* Clarity Script */}
-      {CLARITY_ID && (
-        <Script
-          id="clarity-init"
-          strategy="afterInteractive"
-        >
-          {`(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/${CLARITY_ID}";y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script");`}
-        </Script>
-      )}
+      <Script
+        id="clarity-init"
+        strategy="afterInteractive"
+      >
+        {`(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/${CLARITY_ID}";y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script");`}
+      </Script>
     </>
   );
 }
