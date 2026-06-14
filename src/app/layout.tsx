43d23599d4b1c1767df24291bc20d6c0b39b3ad7
Paresh Solanki <solankiparesh1183@gmail.com>
2026-06-14 14:47:51 +0530

    fix: use official next/third-parties GoogleTagManager to fix tag coverage on dynamic routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from"next";
 import Script from"next/script";
+import { GoogleTagManager } from '@next/third-parties/google';
 import { Instrument_Sans, Space_Grotesk } from"next/font/google";
 import"./globals.css";
 import OrganizationSchema from"@/components/common/OrganizationSchema";
@@ -82,19 +83,7 @@ export default function RootLayout({
  return (
  <html lang="en" suppressHydrationWarning>
  <head>
-  <Script
-    id="gtm-script"
-    strategy="afterInteractive"
-    dangerouslySetInnerHTML={{
-      __html: `
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${GTM_CONTAINER_ID}');
-      `,
-    }}
-  />
+  <GoogleTagManager gtmId={GTM_CONTAINER_ID} />
   <Script
     id="adsense-script"
     strategy="afterInteractive"
@@ -105,16 +94,6 @@ export default function RootLayout({
  <body className={`${instrumentSans.variable} ${spaceGrotesk.variable} font-sans antialiased`} suppressHydrationWarning>
  <OrganizationSchema />
  <Analytics />
-
- <noscript>
- <iframe
- src={`https://www.googletagmanager.com/ns.html?id=${GTM_CONTAINER_ID}`}
- height="0"
- width="0"
- style={{ display:"none", visibility:"hidden" }}
- aria-hidden="true"
- />
- </noscript>
 
  <SafeClientLayout>{children}</SafeClientLayout>
  </body>
